666a451efe07878542b73f0455a91bb20b3d41a2
Travis Dever <tdever@aviatrix.com>
2021-11-09 10:04:13 -0600

    pushing updates

--- modules/azure/aviatrix_controller/outputs.tf
+++ modules/azure/aviatrix_controller/outputs.tf
@@ -1,6 +1,7 @@
 output "controller_public_ip" {
   value       = azurerm_linux_virtual_machine.aviatrix_controller_vm.public_ip_address
   description = "The IP Address of the Aviatrix Controller"
+  sensitive   = true
 }
 
 output "controller_subnet_id" {
@@ -9,10 +10,15 @@ output "controller_subnet_id" {
 }
 
 output "controller_security_group_name" {
-  value = azurerm_network_security_group.controller_security_group.name
+  value       = azurerm_network_security_group.controller_security_group.name
   description = "The Controllers network security group"
+  sensitive   = true
 }
 output "controller_resource_group_name" {
-  value = azurerm_resource_group.resource_group.name
+  value       = azurerm_resource_group.resource_group.name
   description = "The resource group name of the controller"
+}
+output "user_public_ip_address" {
+  value       = var.controller_user_public_ip_address
+  description = "The public IP address of the User; used for NSG rules"
 }
