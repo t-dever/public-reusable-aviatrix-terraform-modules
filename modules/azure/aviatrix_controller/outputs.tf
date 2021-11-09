e003cd94a78b217b2d2ad23a465849745f1c2036
t-dever <91967792+t-dever@users.noreply.github.com>
2021-11-09 10:13:00 -0600

    Features/add azure controller (#2)
    Adding azure controller deployment module

---
+++ modules/azure/aviatrix_controller/outputs.tf
@@ -0,0 +1,25 @@
+output "controller_public_ip" {
+  value       = azurerm_linux_virtual_machine.aviatrix_controller_vm.public_ip_address
+  description = "The IP Address of the Aviatrix Controller"
+  sensitive   = true
+}
+
+output "controller_subnet_id" {
+  value       = azurerm_subnet.azure_controller_subnet.id
+  description = "The subnet id for controller"
+}
+
+output "controller_security_group_name" {
+  value       = azurerm_network_security_group.controller_security_group.name
+  description = "The Controllers network security group"
+  sensitive   = true
+}
+output "controller_resource_group_name" {
+  value       = azurerm_resource_group.resource_group.name
+  description = "The resource group name of the controller"
+}
+output "user_public_ip_address" {
+  value       = var.controller_user_public_ip_address
+  description = "The public IP address of the User; used for NSG rules"
+  sensitive   = true
+}
---
+++ modules/azure/aviatrix_controller/variables.tf
@@ -0,0 +1,52 @@
+variable "resource_prefix" {
+  description = "The prefix to the resource group that will be used for all created resources"
+}
+variable "location" {
+  description = "Location of the resource group"
+}
+variable "vnet_address_prefix" {
+  description = "The address prefix used for the vnet e.g. 10.0.0.0/22"
+}
+variable "admin_email" {
+  description = "The email address used for the aviatrix controller registration."
+  sensitive = true
+}
+variable "controller_customer_id" {
+  description = "The customer id for the aviatrix controller"
+  sensitive = true
+}
+variable "controller_admin_password" {
+  description = "The password used for the admin account on the aviatrix controller."
+  sensitive = true
+}
+variable "controller_subnet_address_prefix" {
+  description = "The subnet address prefix that's used for the controller and copilot VMs. e.g. 10.0.0.0/24"
+}
+variable "controller_user_public_ip_address" {
+  description = "The public IP address of the user that is logging into the controller"
+  sensitive = true
+}
+variable "network_watcher_name" {
+  description = "The name of the network watcher instance for nsg flow logs."
+}
+variable "aviatrix_azure_access_account_name" {
+  description = "The account used to manage the aviatrix controller in azure"
+  sensitive = true
+}
+variable "azure_application_key" {
+  description = "The application/client secret/key to perform a backup restore"
+  sensitive = true
+}
+variable "log_analytics_workspace_id" {
+  description = "The log analytics workspace id."
+  sensitive = true
+}
+variable "log_analytics_location" {
+  description = "The log analytics location."
+}
+variable "log_analytics_id" {
+  description = "The log analytics id."
+}
+variable "controller_version" {
+  default = "UserConnect-6.5.2613"
+}
