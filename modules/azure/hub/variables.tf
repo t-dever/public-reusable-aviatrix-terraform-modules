fde1fb1a2a7737dea4c95b56c527a20bc077d29c
Travis Dever <tdever@aviatrix.com>
2022-01-14 11:25:09 -0600

    testing fireall image

--- modules/azure/hub/variables.tf
+++ modules/azure/hub/variables.tf
@@ -65,15 +65,15 @@ variable "firenet_enabled" {
 
 variable "firewall_image" {
   type = object({
-    firewall_image = string
+    firewall_image         = string
     firewall_image_version = string
-    firewall_size = string
-    firewall_username = string
+    firewall_size          = string
+    firewall_username      = string
   })
   default = {
-    firewall_image = "Palo Alto Networks VM-Series Next-Generation Firewall Bundle 1"
+    firewall_image         = "Palo Alto Networks VM-Series Next-Generation Firewall Bundle 1"
     firewall_image_version = "9.1.0"
     firewall_size          = "Standard_D3_v2"
-    username               = "paloAdmin"
+    firewall_username      = "testAdmin"
   }
 }
