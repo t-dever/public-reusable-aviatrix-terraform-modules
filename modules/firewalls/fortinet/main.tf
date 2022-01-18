dde9e91cc31f0af59c20e9777fb088a4c0c49584
Travis Dever <tdever@aviatrix.com>
2022-01-18 14:45:04 -0600

    modifying output

--- modules/firewalls/fortinet/main.tf
+++ modules/firewalls/fortinet/main.tf
@@ -1,17 +1,11 @@
 resource "fortios_system_interface" "trname" {
   algorithm    = "L4"
-  defaultgw    = "enable"
-  distance     = 5
-  ip           = "0.0.0.0 0.0.0.0"
   mtu          = 1500
   mtu_override = "disable"
   name         = "port2"
   type         = "physical"
   vdom         = "root"
   mode         = "dhcp"
-  snmp_index   = 3
   description  = "Created by Terraform Provider for FortiOS"
-  ipv6 {
-    nd_mode = "basic"
-  }
+  allowaccess = "HTTPS"
 }
