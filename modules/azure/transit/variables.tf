013663462737d8896d4591df6b5c84e833ace96a
Travis Dever <tdever@aviatrix.com>
2022-01-21 14:44:24 -0600

    testing egress

--- modules/azure/transit/variables.tf
+++ modules/azure/transit/variables.tf
@@ -149,7 +149,7 @@ locals {
   transit_gateway_subnet     = local.subnets[0]
   transit_gateway_ha_subnet  = local.subnets[1]
   firewall_subnet            = local.subnets[2]
-  fortinet_bootstrap         = local.is_fortinet && var.egress_enabled ? templatefile("${path.module}/firewalls/fortinet/fortinet_init.tftpl", { lan_gateway = local.firewall_lan_subnet, egress = local.firewall_wan_gateway }) : templatefile("${path.module}/firewalls/fortinet/fortinet_init.tftpl", { lan_gateway = local.firewall_lan_subnet, egress = "" })
+  fortinet_bootstrap         = local.is_fortinet && var.egress_enabled ? templatefile("${path.module}/firewalls/fortinet/fortinet_init.tftpl", { lan_gateway = local.firewall_lan_gateway, egress = local.firewall_wan_gateway }) : templatefile("${path.module}/firewalls/fortinet/fortinet_init.tftpl", { lan_gateway = local.firewall_lan_gateway, egress = "" })
   firewall_lan_gateway        = cidrhost(local.subnets[4], 1)
   firewall_wan_gateway        = cidrhost(local.firewall_subnet, 1)
   fortinet_egress            = <<-EOT
