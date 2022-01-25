439ebb68d42023b1ce140e9b765a0e3670ead39e
Travis Dever <tdever@aviatrix.com>
2022-01-24 19:20:22 -0600

    adding additonal output

--- modules/azure/aviatrix_controller/configuration/outputs.tf
+++ modules/azure/aviatrix_controller/configuration/outputs.tf
@@ -1,3 +1,9 @@
 output "aviatrix_azure_account" {
+  description = "The name of the account added."
   value = var.azure_account_name
+}
+
+output "user_public_ip_address" {
+  value = var.controller_user_public_ip_address
+  description = "The ip address of the user accessing the stuff."
 }
